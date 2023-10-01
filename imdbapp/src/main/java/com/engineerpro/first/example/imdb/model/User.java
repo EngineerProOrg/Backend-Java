aa9c9c215113093c47b4a5481bfe2a77a1fd65de
Manh <xuanmanhdieu@gmail.com>
2023-10-01 23:36:00 +0700

    imdb app

---
+++ imdbapp/src/main/java/com/engineerpro/first/example/imdb/model/User.java
@@ -0,0 +1,47 @@
+package com.engineerpro.first.example.imdb.model;
+
+public class User {
+  private int id;
+  private String name;
+  private String gender;
+
+  public User(int id, String name, String gender) {
+    this.id = id;
+    this.name = name;
+    this.gender = gender;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getGender() {
+    return this.gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+        " id='" + getId() + "'" +
+        ", name='" + getName() + "'" +
+        ", gender='" + getGender() + "'" +
+        "}";
+  }
+
+}
